16760e9eec39e7159a099890195cc158395f47c7
netanel <netanel.attali@gmail.com>
2018-12-07 01:01:14 -0800

    changed to v10

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
+
+echo Run 'node Server.js &' to start the server.
