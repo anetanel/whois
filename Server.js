60b8eb8c3c56d8de4d246f7ed8fe0f3df44c9bbe
netanel <netanel.attali@gmail.com>
2018-12-06 23:48:14 -0800

    added deeper lookup and cleaned up a bit

--- Server.js
+++ Server.js
@@ -3,46 +3,52 @@ const net = require('net');
 
 const WHOIS_ROOT = 'whois.iana.org';
 const PORT = 43;
-const USER_PASS = {"netanel": "1234", "admin": "admin", "hax0r": "password"};
+const USER_PASS = {
+    'netanel': '1234',
+    'admin': 'admin',
+    'hax0r': 'password',
+    'user': 'pass'
+};
 
 http.createServer((req, res) => { // Start http server and call wClient for each request
-    if (authenticate(req, res)) {
-        let target = req.url.substr(1);
+    if (!authenticate(req, res)) return;
+
+    let target = getIpFromUrl(req.url);
+    if (target) {
+        res.write(`IP in url is '${target}'\n`);
         wClient(WHOIS_ROOT, target, res)
+    } else {
+        res.end(`Could not find a valid IP address in '${req.url}'\n`);
     }
 }).listen(8080);
 
 function authenticate(req, res) {
     let auth = req.headers['authorization'];
-    console.log("Authorization Header is: ", auth);
-    if (!auth) {
+    // console.log('Authorization Header is: ', auth);
+
+    if (!auth) { // no authentication given
         res.statusCode = 401;
         res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
-        res.end('Need some creds son');
+        res.end(`No authentication given\nTry 'username:password@${req.headers.host}${req.url}'\n`);
         return false;
-    } else if (auth) {
-        let tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
-        let buf = Buffer.from(tmp[1], 'base64');
-        let plain_auth = buf.toString();        // read it back out as a string
-
-        console.log("Decoded Authorization ", plain_auth);
-        let creds = plain_auth.split(':');      // split on a ':'
-        let username = creds[0];
-        let password = creds[1];
-
-        if (USER_PASS[username] === password) {   // Is the username/password correct?
-            res.statusCode = 200;  // OK
-            return true;
-        } else {
-            // res.statusCode = 401; // Force them to retry authentication
-            // res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
-
-            res.statusCode = 403;   // or alternatively just reject them altogether with a 403 Forbidden
-
-            res.end('You shall not pass\n');
-            return false;
-        }
     }
+
+    let tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+    let buf = Buffer.from(tmp[1], 'base64');
+    let plainAuth = buf.toString();
+
+    // console.log('Decoded Authorization ', plainAuth);
+    let creds = plainAuth.split(':');
+    let username = creds[0];
+    let password = creds[1];
+
+    if (USER_PASS[username] !== password) {
+        res.statusCode = 403;
+        res.end('You shall not pass\n');
+        return false;
+    }
+    res.statusCode = 200;
+    return true;
 }
 
 function wClient(refer, target, res) {
@@ -54,18 +60,26 @@ function wClient(refer, target, res) {
     });
 
     socket.on('end', () => { // Got all data from the server
-        let pretty_data = data.replace(/[%#].*\n/g, '').replace(/\n{2,}/g, "\n");
-        res.write('\n---------------START----------------\n');
-        res.write(pretty_data);
-        res.write('\n---------------END------------------\n');
-        console.log(data);
+        let prettyData = data.replace(/[%#].*\n/g, '')
+            .replace(/\n+/g, '\n')
+            .replace(/^\n|\n$/g, ''); // remove comments and empty lines
 
-        if (data.search('refer:') > 0) { // found refer in data string
-            let next_refer = data.toString().match('refer:.*\n')[0].split(":")[1].trim() // isolate fefer address
-            console.log(`got next refer: ${next_refer}`);
-            wClient(next_refer, target, res)
+        res.write('\n---------------START----------------\n');
+        res.write(prettyData);
+        res.write('\n---------------END------------------\n');
+        // console.log(data);
+
+        if (data.search('refer:') > 0) { // found refer: in data string, so querying it for our target ip
+            let nextRefer = data.match('refer:.*\n')[0].split(':')[1].trim(); // isolate refer address
+            // console.log(`got next refer: ${nextRefer}`);
+            wClient(nextRefer, target, res);
+        } else if (prettyData.search(':') < 0) { // data does not look like key-value. query again with handle/id instead of IP
+            // console.log('digging deeper...');
+            let nextTarget = prettyData.split('\n').slice(-1)[0].split(/[()]/)[1]; // extract handle/id from parentheses of last entry
+            // console.log(`nextTarget = ${nextTarget}`);
+            wClient(refer, nextTarget, res);
         } else {
-            console.log('made it to the end..');
+            // console.log('made it to the end..');
             socket.destroy();
             res.end();
         }
@@ -77,13 +91,18 @@ function wClient(refer, target, res) {
 
     socket.on('error', err => {
         console.error('error: ', err);
-        res.write(`Error getting ${target} whois information.\n${err.toString()}`);
-        res.end();
+        res.end(`Error getting ${target} whois information.\n${err.toString()}\n`);
     });
 
     socket.connect(PORT, refer, () => {
-        console.log(`whois server: ${refer}, target: ${target}`);
+        // console.log(`whois server: ${refer}, target: ${target}`);
         socket.write(target + '\r\n');
     });
+}
+
+function getIpFromUrl(url) {
+    let ipRegex = /\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b/; // check https://www.regular-expressions.info/ip.html
+    let ip = url.match(ipRegex);
+    return ip === null ? false : ip[0];
 }
 
