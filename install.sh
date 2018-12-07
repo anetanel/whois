5d6215b7dbb8e8aa47a4cd7ff93499715da511d2
netanel <netanel.attali@gmail.com>
2018-12-07 01:00:08 -0800

    changed to v10

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
